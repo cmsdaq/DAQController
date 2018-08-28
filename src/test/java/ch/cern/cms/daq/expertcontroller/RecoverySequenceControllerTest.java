2182d34d0007a54146bdf4ef8babd02af9e1ae11
Maciej <maciej.gladki@gmail.com>
2018-08-28 11:50:41 +0200

    test(): update tests with ttchr, accelerate tests

--- src/test/java/ch/cern/cms/daq/expertcontroller/RecoverySequenceControllerTest.java
+++ src/test/java/ch/cern/cms/daq/expertcontroller/RecoverySequenceControllerTest.java
@@ -2,12 +2,14 @@ package ch.cern.cms.daq.expertcontroller;
 
 import ch.cern.cms.daq.expertcontroller.api.RecoveryRequest;
 import ch.cern.cms.daq.expertcontroller.api.RecoveryRequestStep;
+import ch.cern.cms.daq.expertcontroller.api.RecoveryResponse;
 import ch.cern.cms.daq.expertcontroller.persistence.RecoveryRecord;
 import ch.cern.cms.daq.expertcontroller.persistence.RecoveryRecordRepository;
 import ch.cern.cms.daq.expertcontroller.rcmsController.LV0AutomatorControlException;
+import ch.cern.cms.daq.expertcontroller.rcmsController.LV0AutomatorController;
 import ch.cern.cms.daq.expertcontroller.rcmsController.RcmsController;
 import org.h2.tools.Recover;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -42,16 +44,36 @@ public class RecoverySequenceControllerTest {
     @Autowired
     RecoveryRecordRepository recoveryRecordRepository;
 
+    @Autowired
+    RecoveryManager recoveryManager;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.setProperty("observe.period", "1000");
+    }
+
+
+    /**
+     * Regular sequence of signals is tested here: start -> accept -> problem-finished -> stepCompleted -> end
+     * In between signals the entries in repository are asserted
+     */
     @Test
-    public void test() {
+    public void reqularUseCaseTest() {
 
+        System.out.println(recoverySequenceController);
+        System.out.println(recoveryManager);
+
+        // check initial state of repository
         Date start = new Date();
-
         List<RecoveryRecord> all = recoveryRecordRepository.findAll();
         System.out.println(all);
         assertEquals(0, all.size());
 
+        // step 1: issue a recovery and check repository
         recoverySequenceController.start(genereateRecoveryRequest(1L));
+        recoveryManager.getOngoingProblems().add(1L);
+
+
         all = recoveryRecordRepository.findAll();
         System.out.println(all);
         assertEquals(2, all.size());
@@ -74,6 +96,7 @@ public class RecoverySequenceControllerTest {
         assertEquals(2, all.size());
 
 
+        // step 2: accept the recovery and check the repository
         recoverySequenceController.accept(1910902,"");
         all = recoveryRecordRepository.findAll();
         System.out.println(all);
@@ -89,10 +112,18 @@ public class RecoverySequenceControllerTest {
                 hasProperty("end", notNullValue()))
         ));
 
-        recoverySequenceController.stepCompleted();
+
+        // step 3: signal from RCMS - recovery completed
+        recoverySequenceController.stepCompleted(1L);
         all = recoveryRecordRepository.findAll();
         System.out.println(all);
         assertEquals(4, all.size());
+
+        assertThat("Main recovery still not finished",all, hasItem(allOf(
+                hasProperty("name", startsWith("Recovery of")),
+                hasProperty("end", nullValue()))
+        ));
+
         assertThat(all, contains(
                 hasProperty("name", startsWith("Recovery of")),
                 hasProperty("name", is("Waiting for approval")),
@@ -100,14 +131,25 @@ public class RecoverySequenceControllerTest {
                 hasProperty("name", startsWith("Observing"))
         ));
 
+
+
         assertThat(all, hasItem(allOf(
                 hasProperty("name", startsWith("Executing ")),
                 hasProperty("start", notNullValue()),
                 hasProperty("end", notNullValue()))
         ));
 
+        // step 4: signal from expert - problem finished
+        recoveryManager.finished(1L);
 
-        recoverySequenceController.end();
+        // step 5: signal from self (see observe period) - no more actions needed
+        // recoverySequenceController.end(); - do not call this explicitely - this will be called after observe period ends.
+        // This will happen automatically after 1 sec ( see RecoverySequenceControllerMock)
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         all = recoveryRecordRepository.findAll();
         System.out.println(all);
         assertEquals(4, all.size());
@@ -131,6 +173,83 @@ public class RecoverySequenceControllerTest {
 
     }
 
+
+    /**
+     * Regular sequence of signals is tested here: start -> accept -> stepCompleted -> end
+     * In between signals the entries in repository are asserted
+     */
+    @Test
+    public void problemFinishesSignalNeverReceivedFromTheExpertTest() {
+
+        // check initial state of repository
+        Date start = new Date();
+        List<RecoveryRecord> all = recoveryRecordRepository.findAll();
+        assertEquals(0, all.size());
+
+        // step 1: issue a recovery and check repository
+        recoverySequenceController.start(genereateRecoveryRequest(2L));
+        recoveryManager.getOngoingProblems().add(2L);
+
+
+        all = recoveryRecordRepository.findAll();
+        assertEquals(2, all.size());
+        Date end = new Date();
+
+        all = recoveryRecordRepository.findBetween(start, end);
+        assertEquals(2, all.size());
+
+
+        // step 2: accept the recovery and check the repository
+        recoverySequenceController.accept(1910902,"");
+        all = recoveryRecordRepository.findAll();
+        assertEquals(3, all.size());
+
+
+        // step 3: signal from RCMS - recovery completed
+        recoverySequenceController.stepCompleted(2L);
+        all = recoveryRecordRepository.findAll();
+        assertEquals(4, all.size());
+
+        // step 4: signal from expert DOESN'T COME - problem still ongoing
+        // recoveryManager.finished(2L); - don't call this - just for demonstration what is missing in this test case
+
+        // step 5: signal from self (see observe period) - no more actions needed
+        // recoverySequenceController.end(); - do not call - just for demonstration what is missing in this test case
+        // This will happen automatically after 1 sec ( see RecoverySequenceControllerMock)
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        all = recoveryRecordRepository.findAll();
+        System.out.println(all);
+        assertEquals(4, all.size());
+
+        assertThat(all, contains(
+                hasProperty("name", startsWith("Recovery of")),
+                hasProperty("name", is("Waiting for approval")),
+                hasProperty("name", startsWith("Executing ")),
+                hasProperty("name", startsWith("Observing"))
+        ));
+        assertThat("Main recovery entry still ongoing",all, hasItem(allOf(
+                hasProperty("name", startsWith("Recovery of")),
+                hasProperty("start", notNullValue()),
+                hasProperty("end", nullValue()))
+        ));
+
+
+        end = new Date();
+        all = recoveryRecordRepository.findBetween(start, end);
+        assertEquals(4, all.size());
+
+    }
+
+    @After
+    public void beforeTest(){
+        recoveryRecordRepository.deleteAllInBatch();
+    }
+
     private RecoveryRequest genereateRecoveryRequest(Long id){
 
         RecoveryRequest rr = new RecoveryRequest();
@@ -139,6 +258,9 @@ public class RecoverySequenceControllerTest {
 
     }
 
+    /**
+     * This TestConfig will be loaded by spring in order to replace RecoveryManager with it's mock version defined below
+     */
     @TestConfiguration
     public static class TestConfig {
         @Bean
@@ -146,11 +268,47 @@ public class RecoverySequenceControllerTest {
             return new RecoverySequenceControllerTest.TestConfig.RecoveryManagerMock();
         }
 
+        @Bean
+        public RecoverySequenceController recoverySequenceController(){
+            return new RecoverySequenceControllerTest.TestConfig.RecoverySequenceControllerMock();
+        }
+
+        @Bean
+        public LV0AutomatorController lv0AutomatorController() throws LV0AutomatorControlException {
+            return new RecoverySequenceControllerTest.TestConfig.LV0AutomatorControllerMock(null);
+        }
+
         public class RecoveryManagerMock extends RecoveryManager {
 
-
+            /**
+             * This method of RecoveryManager is overwritten in order to prevent from calling other system functions which are not a scope of this test
+             */
             @Override
             public void endRecovery() {
+
+            }
+
+            @Override
+            public void finished(Long id) {
+                getOngoingProblems().remove(id);
+            }
+
+        }
+
+        public class RecoverySequenceControllerMock extends RecoverySequenceController{
+            public RecoverySequenceControllerMock(){
+                observePeriod = 1000;
+            }
+        }
+
+        public class LV0AutomatorControllerMock extends LV0AutomatorController{
+
+            public LV0AutomatorControllerMock(String senderURI) throws LV0AutomatorControlException {
+                super(senderURI);
+            }
+
+            @Override
+            public void interruptRecovery() throws LV0AutomatorControlException {
 
             }
         }
